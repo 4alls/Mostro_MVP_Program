a81a383706d6f96f3dcdc3205bda7c105b0be3b9
eibarraf <105727501+Enricrypto@users.noreply.github.com>
2025-10-29 16:03:41 +0100

    fix: mod.rs and library.rs config (#23)

--- programs/mostro_program/src/instructions/mod.rs
+++ programs/mostro_program/src/instructions/mod.rs
@@ -6,15 +6,17 @@ pub mod create_config;
 pub mod create_proposal;
 pub mod vote_proposal;
 pub mod finalize_proposal;
+pub mod buy_tokens_for_proposal;
 pub mod release_tokens;
 
 // -----------------------------
 // Instruction handlers & accounts
 // -----------------------------
-// This allows `lib.rs` to do `use instructions::*;` and access everything
-pub use create_artist::*; // create_artist_handler, CreateArtist
-pub use create_config::*; // create_config_handler, CreateConfig
-pub use create_proposal::*; // create_proposal_handler, CreateProposal
-pub use vote_proposal::*; // vote_on_proposal_handler, VoteOnProposal
-pub use finalize_proposal::*; // finalize_proposal_handler, FinalizeProposal
-pub use release_tokens::*; // release_tokens_to_artist_handler, ReleaseTokensToArtist
+// Re-export all handlers and context structs for convenient use in `lib.rs`
+pub use create_artist::{create_artist_handler, CreateArtist};
+pub use create_config::{create_config_handler, CreateConfig};
+pub use create_proposal::{create_proposal_handler, CreateProposal};
+pub use vote_proposal::{vote_proposal_handler, VoteProposal};
+pub use finalize_proposal::{finalize_proposal_handler, FinalizeProposal};
+pub use buy_tokens_for_proposal::{buy_tokens_for_proposal_handler, BuyTokensForProposal};
+pub use release_tokens::{release_tokens_handler, ReleaseTokens};
