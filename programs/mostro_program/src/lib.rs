cfc1b9d3357a26ca280f5a9d0baf464d8211e73b
Enricrypto <105727501+Enricrypto@users.noreply.github.com>
2025-10-30 10:37:14 +0100

    fix: imports for program

--- programs/mostro_program/src/lib.rs
+++ programs/mostro_program/src/lib.rs
@@ -9,6 +9,8 @@ pub mod error;
 pub mod instructions;
 pub mod state;
 
+declare_id!("2SYi3NFHTnCXHEzxNpa8nEyehkmZPyikbCarmxngSdTn");
+
 // Explicit imports for #[program]
 use instructions::create_config::{create_config_handler, CreateConfig};
 use instructions::create_artist::{create_artist_handler, CreateArtist};
@@ -18,12 +20,10 @@ use instructions::vote_proposal::{vote_proposal_handler, VoteProposal};
 use instructions::finalize_proposal::{finalize_proposal_handler, FinalizeProposal};
 use instructions::release_tokens::{release_tokens_handler, ReleaseTokens};
 
-declare_id!("2SYi3NFHTnCXHEzxNpa8nEyehkmZPyikbCarmxngSdTn");
-
 /// -----------------------------
 /// Anchor program entrypoint
 /// -----------------------------
-// #[cfg(feature = "anchor")]
+#[cfg(feature = "anchor")]
 #[program]
 pub mod mostro_program {
     use super::*;
