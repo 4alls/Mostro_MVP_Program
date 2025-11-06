6f85829ec57a62276ede99393709bb6e4ffe61c8
Thibaut <38424348+4alls@users.noreply.github.com>
2025-11-06 16:12:04 +0100

    Create perks (#33)
    * Comments Added on code optimizations
    
    * CreatePerks commit

--- programs/mostro_program/src/state/mod.rs
+++ programs/mostro_program/src/state/mod.rs
@@ -2,8 +2,12 @@ pub mod artist;
 pub mod config;
 pub mod proposal;
 pub mod vote;
+pub mod perk;
 
 pub use artist::*;
 pub use config::*;
 pub use proposal::*;
 pub use vote::*;
+pub use perk::*;
+
+
